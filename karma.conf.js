8c66d9e9b528e4b74f2aab4f184f7ec90f712b24
bill wood <billwood.dev@gmail.com>
2016-07-25 21:24:18 -0400

    Modify for a working index page in app

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,31 @@
-module.exports = function(config){
-    config.set({
-        basePath: './app',
+//jshint strict: false
+module.exports = function(config) {
+  config.set({
 
-        files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-animate/angular-animate.js',
-            'bower_components/angular-resource/angular-resource.js',
-            'bower_components/angular-route/angular-route.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            '**/*.module.js',
-            '*!(.module|-test).js',
-            '!(bower_components)/**/*!(.module|-test).js',
-            '**/*-test.js'
-        ],
+    basePath: './app',
 
-        autoWatch: true,
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      '**/*.module.js',
+      '*!(.module|.spec).js',
+      '!(bower_components)/**/*!(.module|.spec).js',
+      '**/*.spec.js'
+    ],
 
-        frameworks: ['jasmine'],
+    autoWatch: true,
 
-        browsers: ['Chrome'],
+    frameworks: ['jasmine'],
 
-        plugin:[
-            'karma-chrome-launcher',
-            'karma-jasmine'
-        ]
+    browsers: ['Chrome'],
 
-    });
-}
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ]
+
+  });
+};
